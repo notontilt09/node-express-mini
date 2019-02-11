572e8ab420e8d33b2c524f896337edde28e22294
Daniel Weinman <namniewd@gmail.com>
2019-02-11 16:45:48 -0500

    added endpoint for getting user with specific id

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ const server = express();
 server.use(express.json());
 
 // routes === endpoints
+
+// handle GET request for all users
 server.get('/api/users', (req, res) => {
     db.find()
         .then(users => {
@@ -14,6 +16,19 @@ server.get('/api/users', (req, res) => {
         })
         .catch(err => {
             res.status(err.code).json({success: false, message: err.message})
+        })
+});
+
+// handle GET request for specific user
+server.get('/api/users/:id', (req, res) => {
+    const userId = req.params.id;
+
+    db.findById(userId)
+        .then(user => {
+            res.status(200).json({success: true, user})
+        })
+        .catch(err => {
+            res.status(500).json({success: false, err})
         })
 });
 
