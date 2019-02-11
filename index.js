c8148bc73f44a5a458cc672d666abc097ae73c94
Daniel Weinman <namniewd@gmail.com>
2019-02-11 16:38:11 -0500

    wrote endpoint to get all users from db

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // implement your API here
+const express = require('express');
+const db = require('./data/db.js');
+const server = express();
+
+//middleware 
+server.use(express.json());
+
+// routes === endpoints
+server.get('/api/users', (req, res) => {
+    db.find()
+        .then(users => {
+            res.status(200).json({success: true, users})
+        })
+        .catch(err => {
+            res.status(err.code).json({success: false, message: err.message})
+        })
+});
+
+
+
+
+
+server.listen(5000, () => {
+    console.log('\n*** Running on port 5000 ***\n');
+})
