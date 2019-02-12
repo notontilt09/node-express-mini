f14d11b9e1122cfe3d36262aba86075fd9451b2a
Daniel Weinman <namniewd@gmail.com>
2019-02-11 19:47:55 -0500

    update user functionality added, all stretch goals complete

--- client/src/App.js
+++ client/src/App.js
@@ -10,8 +10,10 @@ class App extends Component {
       users: [],
       newUser: {
         name: '',
-        bio: ''
-      }
+        bio: '',
+      },
+      isUpdating: false,
+      updatingId: null
     }
   }
 
@@ -33,8 +35,7 @@ class App extends Component {
     })
   }
 
-  addUser = (e, user) => {
-    e.preventDefault();
+  addUser = user => {
     axios.post('http://localhost:5000/api/users', user)
         .then(res => {
           axios.get('http://localhost:5000/api/users')
@@ -50,32 +51,82 @@ class App extends Component {
         })
   }
 
-  deleteUser = id => {
-
+  deleteUser = (e, id) => {
+    e.preventDefault();
+    axios.delete(`http://localhost:5000/api/users/${id}`)
+      .then(res => {
+        axios.get('http://localhost:5000/api/users')
+          .then(res => {
+            this.setState({
+              users: res.data.users
+            })
+          })
+      });
   }
 
-  updateUser = (user, id) => {
+  updateUser = () => {
+    const toBeUpdated = this.state.users.find(x => x.id === this.state.updatingId);
+    console.log(toBeUpdated);
+    axios.put(`http://localhost:5000/api/users/${toBeUpdated.id}`, this.state.newUser)
+      .then(res => {
+        axios.get('http://localhost:5000/api/users')
+          .then(res => {
+            this.setState({
+              users: res.data.users
+            })
+          })
+      })
+  }
 
+  populateForm = (e, user) => {
+    e.preventDefault();
+    this.setState({
+      newUser: {
+        name: user.name,
+        bio: user.bio,
+      },
+      isUpdating: true,
+      updatingId: user.id
+    })
+  }
+
+  handleSubmit = e => {
+    e.preventDefault();
+    if (this.state.isUpdating) {
+      this.updateUser()
+      this.setState({
+        isUpdating: false,
+        newUser: {
+          name: '',
+          bio: ''
+        },
+        updatingId: null
+      })
+    } else {
+      this.addUser(this.state.newUser)
+    }
   }
 
   render() {
     return (
       <div className="App">
         <>
-          <form onSubmit={(e) => this.addUser(e, this.state.newUser)}>
+          <form onSubmit={this.handleSubmit}>
             <input 
               type='text' 
               name='name' 
               onChange={this.handleChange} 
               value={this.state.newUser.name}
+              placeholder='name'
             />
             <input 
               type='text' 
               name='bio' 
               onChange={this.handleChange} 
               value={this.state.newUser.bio}
+              placeholder='bio'
             />
-            <button type='submit'>Add</button>
+            <button type='submit'>{this.state.isUpdating ? 'Update' : 'Add'}</button>
           </form>
           <div className='users'>
             {this.state.users.map(user => {
@@ -84,8 +135,8 @@ class App extends Component {
                   <h2>{user.name}</h2>
                   <h3>{user.bio}</h3>
                   <div className='modify'>
-                    <button onClick={this.deleteUser}>Delete</button>
-                    <button onClick={this.updateUser}>Update</button>
+                    <button onClick={(e) => this.deleteUser(e, user.id)}>Delete</button>
+                    <button onClick={(e) => this.populateForm(e, user)}>Update</button>
                   </div>
                 </div>
               )
